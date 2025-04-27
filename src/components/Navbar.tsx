bd8720fcc34ab514e0a6f076775bfd423faac830
nziza-dev-og <nzizadev@gmail.com>
2025-04-27 19:11:06 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   return (
     <nav className="bg-black bg-opacity-90 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-25">
+        <div className="flex items-center justify-between h-22">
           <Link to="/" className="flex-shrink-0 flex items-center">
           <img src={logoImage} alt="Description of Image" width={180} height={60} />
             <span className="ml-2 text-xl font-bold">RebaFlip</span>
