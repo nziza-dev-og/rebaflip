37e32ab60b5502f45c36eb59ecc6434fb109dcd7
nziza-dev-og <nzizadev@gmail.com>
2025-04-27 18:34:13 +0300

    the add

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex-shrink-0 flex items-center">
-          <img src="/assets/large-removebg-preview.png" alt="Description of Image" size={25} />
+          <img src="/src/assets/large-removebg-preview.png" alt="Description of Image" size={25} />
             <span className="ml-2 text-xl font-bold">RebaFlip</span>
           </Link>
           
