c8902e04631be69b78463a9cb2c16a8e6e0c9ea1
nziza-dev-og <nzizadev@gmail.com>
2025-04-29 11:43:25 +0200

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -189,7 +189,7 @@ export default function ProfilePage() {
                     <input
                       id="opacity-control"
                       type="range"
-                      min="25"
+                      min="15"
                       max="100"
                       step="8"
                       value={themeOpacity}
